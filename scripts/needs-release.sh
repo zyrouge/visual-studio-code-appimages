e13d8511ac749f8d4563a151b19c07c9e95bf3f6
zyrouge <zyrouge@hotmail.com>
2023-11-13 19:07:58 +0530

    ci: fix metadata file path

--- scripts/needs-release.sh
+++ scripts/needs-release.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
-if [ -f ./dist/build-complete.txt ]; then
+self=$(readlink -f "$0")
+here=${self%/*}
+root_dir=$(dirname "${here}")
+
+if [ -f "${root_dir}/dist/build-complete.txt" ]; then
     echo "yes"
 fi
